23ff960f6ddeaf83f330f4c3ddd520e9a26b7304
DESKTOP-0ABCL26\tj-bu <ljm5704@gmail.com>
2022-07-22 16:45:40 +0900

    김희경 2일차 작업

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="./css/sytle.css">
   <link rel="stylesheet" href="./css/header.css">
   <link rel="stylesheet" href="./css/footer.css">
+  <link rel="stylesheet" href="./css/home.css">
   <link rel="stylesheet" href="./css/sub-common.css">
   <link rel="stylesheet" href="./css/page1.css">
   <link rel="stylesheet" href="./css/page2.css">
