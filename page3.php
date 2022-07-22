d9e3c188edfb102a0a623842dd6c599292ee7de7
DESKTOP-ST2AIC0\tj-bu <tjdwlsakfn2@naver.com>
2022-07-22 16:57:31 +0900

    page3 2일차 작업

--- page3.php
+++ page3.php
@@ -1,5 +1,6 @@
 <?php include "sub-header.php" ?>
 <script src="./js/page3.js"></script>
   <p class="lee-page3">page3<p>
+  <p class="lee-page3">page3 2일차 작업<p>
 <?php include "sub-footer.php" ?>
 
