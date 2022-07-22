1bee68c2fa441529d1dbf59c34c3cfe38bb297b2
DESKTOP-ST2AIC0\tj-bu <tjdwlsakfn2@naver.com>
2022-07-22 17:00:22 +0900

    page3 5일차 작업 입니다

--- page3.php
+++ page3.php
@@ -4,5 +4,6 @@
   <p class="lee-page3">page3 2일차 작업<p>
   <p class="lee-page3">page3 3일차 작업<p>
   <p class="lee-page3">page3 4일차 작업<p>
+  <p class="lee-page3">page3 5일차 작업입니다<p>
 <?php include "sub-footer.php" ?>
 
